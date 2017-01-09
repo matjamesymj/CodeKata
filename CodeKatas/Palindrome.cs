dec880fa5bc1af643ed43a6ac35d99456260d42f
Mathew James <matjamesymj@hotmail.com>
2017-01-09 15:00:32 +0000

    added test project and palindrome kata

---
+++ CodeKatas/Palindrome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas
+{
+    /// <summary>
+    /// A palindrome is a word that reads the same backward or forward.
+    /// Write a function that checks if a given word is a palindrome.Character case should be ignored.
+    //For example, IsPalindrome("Deleveled") should return true as character case should be ignored, resulting in "deleveled", which is a palindrome since it reads the same backward and forward.
+    /// </summary>
+    public class Palindrome
+    {
+        public static bool IsPalindromeUsingArray(string word)
+        {
+            var array = word.ToCharArray();
+            Array.Reverse(array);
+            var wordBackwards = new string(array);
+            return string.Equals(word, wordBackwards, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public static bool IsPalindromeUsingLinq(string word)
+        {
+            return word.ToLower() == new string(word.ToLower().Reverse().ToArray());
+        }
+    }
+}
---
+++ CodeKatasTests/PalindromeTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeKatas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatasTests
+{
+    [TestClass()]
+    public class PalindromeTests
+    {
+        [TestMethod()]
+        public void IsPalindromeUsingArrayTest()
+        {
+           Assert.IsTrue(Palindrome.IsPalindromeUsingArray("Deleveled"));
+            Assert.IsFalse(Palindrome.IsPalindromeUsingArray("not"));
+
+
+        }
+
+        [TestMethod()]
+        public void IsPalindromeUsingLinqTest()
+        {
+            Assert.IsTrue(Palindrome.IsPalindromeUsingLinq("Deleveled"));
+            Assert.IsFalse(Palindrome.IsPalindromeUsingLinq("not"));
+        }
+    }
+}
