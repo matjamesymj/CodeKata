043415b1ece9eb92d7b8af3024f0c2fece73128c
Mathew James <matjamesymj@hotmail.com>
2017-01-10 08:42:11 +0000

    TwoSumKata

---
+++ CodeKatas/TwoSum/TwoSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CodeKatas.TwoSum
+{
+    public class TwoSum
+    {
+        public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
+        {
+            //Tuple<int, int> returnValue = null;
+            //for (var firstIndex = 0; firstIndex < list.Count; firstIndex++)
+            //{
+            //    if (firstIndex <= 0) continue;
+            //    var listItem = list[firstIndex];
+            //    for (var secondIndex = 0; secondIndex < list.Count; secondIndex++)
+            //    {
+            //        var item = list[secondIndex];
+            //        if (listItem + item != sum) continue;
+            //        returnValue = new Tuple<int, int>(secondIndex, firstIndex);
+            //        break;
+            //    }
+            //}
+            //return returnValue;
+
+            var result = from n1 in list
+                from n2 in list
+                where n1 + n2 == sum
+                select new Tuple<int, int>(list.IndexOf(n1), list.IndexOf(n2));
+
+            return result.FirstOrDefault();
+
+        }
+
+    }
+}
+    
---
+++ CodeKatasTests/TwoSum/TwoSumTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatasTests.TwoSum
+{
+    [TestClass()]
+    public class TwoSumTests
+    {
+        [TestMethod()]
+        public void FindTwoSumTestIndexValues1And4()
+        {
+            var expected = new Tuple<int,int>(1, 4);
+            Assert.AreEqual(expected,CodeKatas.TwoSum.TwoSum.FindTwoSum(list: new List<int> { 1, 3, 5, 7, 9 }, sum: 12));
+            
+        }
+
+        [TestMethod()]
+        public void FindTwoSumTestShouldReturnNull()
+        {
+            
+            Assert.AreEqual(null, CodeKatas.TwoSum.TwoSum.FindTwoSum(list: new List<int> { 1, 3, 5, 7, 9 }, sum: 3));
+
+        }
+    }
+}
