224a2c9061655baa5826435cbe2111d51b0214ce
Mathew James <matjamesymj@hotmail.com>
2017-01-11 12:14:47 +0000

    bowling game kata in tdd

---
+++ CodeKatas/BowlingGame/Game.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.BowlingGame
+{
+    public class Game
+    {
+        List<int> _rollsPinsHit = new List<int>(21);
+      
+        public void Roll(int pinsHit)
+        {
+            _rollsPinsHit.Add(pinsHit);
+        }
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                int frameIndex = 0;
+                for (int frame = 0; frame < 10; frame++)
+                {
+                    if (IsStrike(frameIndex))
+                    {
+                    score+=10 + StrikeBonus(frameIndex);
+                        frameIndex ++;
+                    }
+                    else if (IsSpare(frameIndex))
+                    {
+                        score += 10 + SpareBonus(frameIndex);
+                        frameIndex += 2;
+                    }
+                    else
+                    {
+                        score += NormalFrameBonus(frameIndex);
+                        frameIndex += 2;
+                    }
+                }
+
+                return score;
+
+
+            }
+        }
+
+        private int NormalFrameBonus(int frameIndex)
+        {
+            return _rollsPinsHit[frameIndex] + _rollsPinsHit[frameIndex + 1];
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return _rollsPinsHit[frameIndex + 2];
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return _rollsPinsHit[frameIndex + 1] + _rollsPinsHit[frameIndex + 2];
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return _rollsPinsHit[frameIndex] == 10;
+        }
+        private bool IsSpare(int frameIndex)
+        {
+            return _rollsPinsHit[frameIndex] + _rollsPinsHit[frameIndex + 1] == 10;
+        }
+    }
+}
---
+++ CodeKatasTests/BowlingGame/BowlingGameTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using CodeKatas.BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatasTests.BowlingGame
+{
+    [TestClass()]
+    public class BowlingGameTests
+    {
+        [TestMethod()]
+        public void TestGutterGameTest()
+        {
+            var bowlingGame = SetUpCame();
+
+            RollPins(bowlingGame,20,0);
+            Assert.AreEqual(0, bowlingGame.Score);
+        }
+
+        
+        [TestMethod()]
+        public void TestHIttingOnePinPerRollTest()
+        {
+            var bowlingGame = new Game();
+
+            RollPins(bowlingGame, 20, 1);
+            Assert.AreEqual(20, bowlingGame.Score);
+        }
+
+        [TestMethod()]
+        public void TestOneSpareTest()
+        {
+            var bowlingGame = new Game();
+
+            RollSpare(bowlingGame);
+            bowlingGame.Roll(3);
+
+            RollPins(bowlingGame, 17, 0);
+            Assert.AreEqual(16, bowlingGame.Score);
+        }
+        [TestMethod()]
+        public void TestOneStrikeTest()
+        {
+            var bowlingGame = new Game();
+
+            RollStrike(bowlingGame);
+            bowlingGame.Roll(3);
+            bowlingGame.Roll(4);
+            RollPins(bowlingGame, 16, 0);
+            Assert.AreEqual(24, bowlingGame.Score);
+        }
+
+        [TestMethod()]
+        public void TestPerfectGameTest()
+        {
+            var bowlingGame = new Game();
+
+            RollPins(bowlingGame, 12, 10);
+            Assert.AreEqual(300, bowlingGame.Score);
+        }
+
+        private static void RollSpare(Game bowlingGame)
+        {
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(5);
+        }
+
+        private static void RollStrike(Game bowlingGame)
+        {
+            bowlingGame.Roll(10);
+        }
+
+
+        private static Game SetUpCame()
+        {
+            return new Game();
+        }
+
+        private static void RollPins(Game bowlingGame, int numberOfRolls, int pinsHitPerRoll)
+        {
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                bowlingGame.Roll(pinsHitPerRoll);
+            }
+        }
+    }
+
+
+}
