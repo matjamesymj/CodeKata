bd1b92d033e31e6a7ef918494bede2616ab17b88
Mathew James <matjamesymj@hotmail.com>
2017-01-11 09:03:13 +0000

    oddeven kata

---
+++ CodeKatas/OddEven/OddEven.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.OddEven
+{
+    public class OddEven
+    {
+        public string Print(int startNumber, int lastNumber)
+        {
+            string result = string.Empty;
+            for (int i = startNumber; i < lastNumber; i++)
+            {
+
+                if (IsPrimeNumber(i))
+                {
+                    result += string.Format(" {0}", i);
+                }
+
+                if (!IsPrimeNumber(i) & IsOddNumber(i))
+                    if (i == startNumber)
+                    {
+                        result += string.Format("{0}" + "Odd", result);
+                    }
+                    else
+                    {
+                        result = string.Format("{0} " + "Odd", result);
+                    }
+
+
+                if (IsEvenNumber(i))
+                {
+                    result = string.Format("{0} Even", result);
+                }
+
+            }
+            return result;
+        }
+        private static bool IsEvenNumber(int number)
+        {
+            return number >= 2 && number % 2 == 0;
+        }
+        private static bool IsOddNumber(int number)
+        {
+            return (number % 2 != 0);
+        }
+        private static bool IsPrimeNumber(int number)
+        {
+            if (number < 2) return false;
+            if (IsEvenNumber(number)) return false;
+            var divisble = 3;
+            while (divisble * divisble <= number)
+            {
+                if (number % divisble == 0) return false;
+                divisble += 2;
+            }
+            return true;
+
+        }
+    }
+}
---
+++ CodeKatasTests/OddEven/OddEvenTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatasTests.OddEven
+{
+    [TestClass()]
+    public class OddEvenTests
+    {
+        [TestMethod()]
+        public void PrintTest1to11()
+        {
+            var oddEven = new CodeKatas.OddEven.OddEven();
+
+            var result = oddEven.Print(1,11);
+          
+            Assert.AreEqual("Odd Even 3 Even 5 Even 7 Even Odd Even", result);
+
+        }
+
+        [TestMethod()]
+        public void PrintTest11to1()
+        {
+            var oddEven = new CodeKatas.OddEven.OddEven();
+
+            var result = oddEven.Print(11, 1);
+
+            Assert.AreEqual("", result);
+
+        }
+    }
+}
