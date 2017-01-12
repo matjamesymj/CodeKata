942581c484c1d017384f4706527f5a1b00c36c30
Mathew James <matjamesymj@hotmail.com>
2017-01-12 09:29:40 +0000

    100 doors kata

---
+++ CodeKatas/100Doors/HundreadDoors.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas._100Doors
+{
+    public class HundreadDoors
+    {
+        List<Door> _doors = new List<Door>(100);
+
+        public HundreadDoors()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _doors.Add(new Door());
+            }
+        }
+
+
+        public string Run()
+        {
+            PassDoorsHundreadTimes();
+            return WhichDoorsAreOpen();
+        }
+
+        public void PassDoorsHundreadTimes()
+        {
+            for (int passIndex = 0; passIndex < 100; passIndex++)
+            {
+                for (int doorIndex = 0; doorIndex < _doors.Count; doorIndex++)
+                {
+                    if ((doorIndex + 1) % (passIndex + 1) == 0)
+                    {
+                        var door = _doors[doorIndex];
+                        ToggleDoor(door);
+                    }
+
+                }
+            }
+
+        }
+
+        public string WhichDoorsAreOpen()
+        {
+            var result = "";
+            for (int doorIndex = 0; doorIndex < _doors.Count; doorIndex++)
+            {
+                var door = _doors[doorIndex];
+                if (door.Open)
+                {
+                    result += string.Format("{0}, ", doorIndex + 1);
+                }
+            }
+            char[] charsToTrim = { ',',' ' };
+            return result.Trim(charsToTrim);
+        }
+
+        private static void ToggleDoor(Door door)
+        {
+            door.Open = !door.Open;
+        }
+    }
+
+    internal class Door
+    {
+        public Door()
+        {
+            Open = false;
+        }
+        public bool Open { get; set; }
+    }
+}
---
+++ CodeKatasTests/100Doors/HundreadDoorsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CodeKatas._100Doors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatasTests._100Doors
+{
+    [TestClass()]
+    public class HundreadDoorsTests
+    {
+        [TestMethod()]
+        public void RunTest()
+        {
+            var hundreadDoors = new HundreadDoors();
+
+          Assert.AreEqual("1, 4, 9, 16, 25, 36, 49, 64, 81, 100",  hundreadDoors.Run());
+            
+        }
+    }
+}
