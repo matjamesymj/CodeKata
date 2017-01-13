376bf300de0b103f4d5df3ef6cca8278e36c3937
Mathew James <matjamesymj@hotmail.com>
2017-01-13 18:23:19 +0000

    roman numeral kata

---
+++ CodeKatas/RomanNumerals/RomanNumeralsUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.RomanNumerals
+{
+    public class RomanNumeralsUtils
+    {
+        public static string ArabicToRoman(int arabic)
+        {
+            var remainingResult = arabic;
+            var stringBuilder = new StringBuilder();
+            int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+  
+            for (var i = 0; i < values.Length; i++)
+            {
+                remainingResult = AppendRomanNumerals(remainingResult, values[i], symbols[i], stringBuilder);
+            }
+
+
+            return stringBuilder.ToString();
+        }
+
+        private static int AppendRomanNumerals(int arabic, int value, string romanNumerals, StringBuilder stringBuilder)
+        {
+            var remainingResult = arabic;
+            while (remainingResult >= value)
+            {
+                stringBuilder.Append(romanNumerals);
+                remainingResult -= value;
+            }
+            return remainingResult;
+        }
+    }
+}
---
+++ CodeKatasTests/RomanNumerals/RomanNumeralsTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using CodeKatas.RomanNumerals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatasTests.RomanNumerals
+{
+    [TestClass()]
+    public class RomanNumeralsTests
+    {
+        [TestMethod()]
+        public void TestReturnValuesFor1to3Passed()
+        {
+            Assert.AreEqual("I", RomanNumeralsUtils.ArabicToRoman(1));
+            Assert.AreEqual("II", RomanNumeralsUtils.ArabicToRoman(2));
+            Assert.AreEqual("III", RomanNumeralsUtils.ArabicToRoman(3));
+        }
+
+        [TestMethod]
+        public void TestReturnValue4()
+        {
+            Assert.AreEqual("IV", RomanNumeralsUtils.ArabicToRoman(4));
+        }
+        [TestMethod]
+        public void TestReturnValue5()
+        {
+            Assert.AreEqual("V", RomanNumeralsUtils.ArabicToRoman(5));
+        }
+
+        [TestMethod]
+        public void TestReturnValue6To8()
+        {
+            Assert.AreEqual("VI", RomanNumeralsUtils.ArabicToRoman(6));
+            Assert.AreEqual("VII", RomanNumeralsUtils.ArabicToRoman(7));
+            Assert.AreEqual("VIII", RomanNumeralsUtils.ArabicToRoman(8));
+        }
+
+        [TestMethod]
+        public void TestReturnValue10()
+        {
+            Assert.AreEqual("X", RomanNumeralsUtils.ArabicToRoman(10));
+        }
+
+        [TestMethod]
+        public void TestReturnValue11()
+        {
+            Assert.AreEqual("XI", RomanNumeralsUtils.ArabicToRoman(11));
+        }
+        [TestMethod]
+        public void TestReturnValue19()
+        {
+            Assert.AreEqual("XIX", RomanNumeralsUtils.ArabicToRoman(19));
+        }
+        [TestMethod]
+        public void TestReturnValue50()
+        {
+            Assert.AreEqual("L", RomanNumeralsUtils.ArabicToRoman(50));
+        }
+
+        [TestMethod]
+        public void TestReturnValue40()
+        {
+            Assert.AreEqual("XL", RomanNumeralsUtils.ArabicToRoman(40));
+        }
+
+        [TestMethod]
+        public void TestReturnValue927()
+        {
+            Assert.AreEqual("CMXXVII", RomanNumeralsUtils.ArabicToRoman(927));
+        }
+
+        [TestMethod]
+        public void TestReturnValue3000()
+        {
+            Assert.AreEqual("MMM", RomanNumeralsUtils.ArabicToRoman(3000));
+        }
+
+
+    }
+}
