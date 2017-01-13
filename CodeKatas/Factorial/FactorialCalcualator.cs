9f87dba6913eadf5811c5bd2e4944f4b2ab4c6ab
Mathew James <matjamesymj@hotmail.com>
2017-01-13 13:03:01 +0000

    facotrial kata

---
+++ CodeKatas/Factorial/FactorialCalcualator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.Factorial
+{
+    public class FactorialCalcualator
+    {
+
+        public double CalculateUsingForLoop(int facotrialNumber)
+        {
+            var facotrialResult = 1;
+
+            for (int i = 1; i <= facotrialNumber; i++)
+            {
+                facotrialResult *= i;
+
+            }
+            return facotrialResult;
+
+
+        }
+
+        public double CalculateUsingRecursion(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+
+            }
+            else
+            {
+                return number * (CalculateUsingRecursion(number - 1));
+            }
+
+        }
+    }
+}
---
+++ CodeKatasTests/Factorial/FactorialTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CodeKatas.Factorial;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKatasTests.Factorial
+{
+    [TestClass()]
+    public class FactorialTests
+    {
+        [TestMethod()]
+        public void CalculateFactorialOfZero()
+        {
+           FactorialCalcualator factorialCalcualator = new FactorialCalcualator();
+
+           Assert.AreEqual(1,factorialCalcualator.CalculateUsingForLoop(0));
+        }
+        [TestMethod()]
+        public void CalculateFactorialOfFour()
+        {
+            FactorialCalcualator factorialCalcualator = new FactorialCalcualator();
+
+            Assert.AreEqual(24, factorialCalcualator.CalculateUsingForLoop(4));
+        }
+
+        [TestMethod()]
+        public void CalculateFactorialUsingRecursionForZero()
+        {
+            FactorialCalcualator factorialCalcualator = new FactorialCalcualator();
+            Assert.AreEqual(1, factorialCalcualator.CalculateUsingRecursion(0));
+        }
+        [TestMethod()]
+        public void CalculateFactorialUsingRecursionForFour()
+        {
+            FactorialCalcualator factorialCalcualator = new FactorialCalcualator();
+            Assert.AreEqual(24, factorialCalcualator.CalculateUsingRecursion(4));
+        }
+    }
+}
