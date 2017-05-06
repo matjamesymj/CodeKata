621743987be95f458935764400888c10c4bd88fb
Mathew James <matjamesymj@hotmail.com>
2017-05-06 07:41:01 +0100

    musical instrument array with contains check to remove vowels

---
+++ CodeKatas/StringArrayMusical/MusicalInstruments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.StringArrayMusical
+{
+    public class MusicalInstruments
+    {
+
+        public List<string> RemoveVowels(String[] musicalInstruments)
+        {
+            var musicalInstrumentsWithoutVowels = new List<string>();
+            foreach (var musicalInstrument in musicalInstruments)
+            {
+                var musicalInstrumentWithoutVowels = musicalInstrument.Replace("A", "").Replace("E","").Replace("I","").Replace("O","").Replace("U","");
+                musicalInstrumentsWithoutVowels.Add(musicalInstrumentWithoutVowels);
+
+            }
+            return musicalInstrumentsWithoutVowels;
+        }
+    }
+}
---
+++ CodeKatasTests/StringArrayMusical/MusicalInstrumentsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKatas.StringArrayMusical;
+
+namespace CodeKatasTests.StringArrayMusical
+{
+    [TestClass()]
+    public class MusicalInstrumentsTests
+    {
+        [TestMethod()]
+        public void RemoveVowelsTest()
+        {
+            var musicalIntruments = new MusicalInstruments();
+            var musicalInstrumentsArray = new string[4];
+            musicalInstrumentsArray[0] = "cello";
+            musicalInstrumentsArray[1] = "guitar";
+            musicalInstrumentsArray[2] = "violin";
+            musicalInstrumentsArray[3] = "double bass";
+            var musicalIntrumentsWithoutVowels = musicalIntruments.RemoveVowels(musicalInstrumentsArray);
+
+            foreach (var musicalInstrumentWithoutVowel in musicalIntrumentsWithoutVowels)
+            {
+                Assert.IsFalse(ContainVowel(musicalInstrumentWithoutVowel));
+                
+            }
+
+        }
+
+        private static bool ContainVowel(string word)
+        {
+            const string vowels ="AEIOU";
+
+            foreach (var vowel in vowels)
+            {
+                if (word.Contains(vowel.ToString()))
+                {
+                    return true;
+                };
+            }
+            return false;
+        }
+    }
+}
